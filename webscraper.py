88b5e6a93d3d01ed602ec2967d1088182a735717
Henry <hhan3@swarthmore.edu>
2017-09-16 17:17:07 -0400

    just need to thread the search and compile the articles

--- webscraper.py
+++ webscraper.py
@@ -1,17 +1,17 @@
 from eventregistry import *
 import numpy as np
 import pandas as pd
+import random
 
 # Print a list of recently added articles mentioning entered words
-api_key = 'eda39267-9017-481a-860d-0b565c6d8bf3'
-er = EventRegistry(apiKey = api_key)
 
-list_of_articles = []
+
+
 def get_articles(claim):
     
     #parse claim into a list of keywords
     keywords = claim
-    print claim
+    list_of_articles = []
     
     q = QueryArticlesIter(keywords=QueryItems.AND(keywords))
     
@@ -28,7 +28,42 @@ def get_articles(claim):
     for article in list_of_articles:
         df = df.append(article,ignore_index=True)
     df.to_csv('articles.csv')
-    
+
+def get_keywords(user_url):
+    url = user_url.decode('utf-8')
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+
+    keywords_unicode = article.keywords
+    keywords = []
+    for word in keywords_unicode:
+        keywords.append(word.encode('utf-8'))
+    return keywords
+
+def get_search_params(keywords):
+    search_params = []
+    while len(keywords) != 0:
+        # Randomly select 3 words
+        rm = random.sample(keywords,3)
+        # add the list of 3 words to the searchable list
+        search_params.append(rm)
+        # remove words from the list
+        for word in rm:
+            keywords.remove(word)
+        # put 1 or 2 random words back
+        if len(keywords) is 3:
+            search_params.append(keywords)
+            keywords = []
+            print "this should be the last thing"
+        elif len(keywords) is 1:
+            keywords.append(random.sample(rm,2)[0:2])
+        else:
+            keywords.append(random.sample(rm,1)[0])
+    return search_params
+
+
 # parameter is a list of strings to query
 get_articles(['mike','hostile','pompeo'])
 
